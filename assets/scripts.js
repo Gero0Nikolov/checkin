8862f89a38bc0f1bbf7329452291acac6409b7a8
Gergan N <gergan.n@logicpromo.com>
2017-03-08 15:24:05 +0200

    WP Code connection x3

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,6 @@
 var FOURSQUARE_VENUE_SEARCH_API_URL = "https://api.foursquare.com/v2/venues/search?";
 var FOURSQUARE_VENUE_SEARCH_API_CID = "353GZNRPJL4HJYGGVHRV1TTE5VUBPFEGWPZT0YZH0FQYMKU4";
+var FOURSQUARE_VENUE_SEARCH_API_CS = "M5LB1RT1DYGN0TRKH1OOGCYRAN5DL3YELS3ZOLR3VSNUHF05";
 var loading = "<div id='loader'></div>";
 
 jQuery( document ).ready(function(){
@@ -16,8 +17,10 @@ jQuery( document ).ready(function(){
 
 function searchVenue( venueTown, venue ) {
     if ( venueTown !== undefined && venueTown != "" ) {
+        jQuery( "#venue-search-container #venues-list" ).append( loading );
+
         jQuery.ajax({
-    		url: FOURSQUARE_VENUE_SEARCH_API_URL +"near="+ venueTown +"&query="+ venue +"&oauth_token="+ FOURSQUARE_VENUE_SEARCH_API_CID +"&v=20170308",
+    		url: FOURSQUARE_VENUE_SEARCH_API_URL +"near="+ venueTown +"&query="+ venue +"&client_id="+ FOURSQUARE_VENUE_SEARCH_API_CID +"&client_secret="+ FOURSQUARE_VENUE_SEARCH_API_CS +"&v=20170308",
     		success: function( result ) {
     			console.log( result );
     		},
